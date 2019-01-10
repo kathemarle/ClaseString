b98b8ce2e4047bd76426dfd2a8481b5b6b0df1a3
marleny <marlenyromerovillca@gmail.com>
2019-01-10 13:18:44 +0100

    metodos lenght() y charArt()

--- src/clasestring/ClaseString.java
+++ src/clasestring/ClaseString.java
@@ -13,6 +13,16 @@ public class ClaseString {
 
    
     public static void main(String[] args) {
+       
+      String s = "iesmaralboran.com";
+      
+      //tamaño de la cadena
+        System.out.println("Tamaño" + s.length());
+        
+        //obtener el char de una posicion concreta
+        
+        System.out.println("Chart asociado a la posicion 5: " + s.charAt(5));
+              
       
     }
     
